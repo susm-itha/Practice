c2257515ec78a24d729c63768aeeab6e0f0a75de
Susm-itha <susmitha.samudrala@nybinfotech.com>
2026-05-29 17:02:23 +0530

    conditions elseif if ifelse switch ternary

---
+++ javascript/Conditions/IF/script.js
@@ -0,0 +1,8 @@
+let age = 15;
+let text = "You can Not drive!";
+
+if ((age >= 18) == true) {
+  text = "You can drive!";
+}
+console.log(text)
+console.log(age >= 18);
---
+++ javascript/Conditions/Ternary/script.js
@@ -0,0 +1,5 @@
+let age = 20;
+let Minor = age < 18;
+let text = (age < 18) ?  "Minor" : "Adult";
+console.log(text)
+console.log(Minor);
