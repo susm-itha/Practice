28925143223915a2f66ea3600d370e3ecf495a8d
Susm-itha <susmitha.samudrala@nybinfotech.com>
2026-07-06 18:16:13 +0530

    06-07-2026

---
+++ JS_Tasks_06-07-2026/React/react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Personal from "./my app/Personal"
+function App(){
+  return(
+    <div>
+      <Personal/>
+    </div>
+  )
+  
+}
+export default App
---
+++ JS_Tasks_06-07-2026/React/react/src/my app/Personal.jsx
@@ -0,0 +1,27 @@
+function personal(){
+    return(
+        <div>
+            <h1>Personal Information</h1>
+
+      <p><b>Name:</b> Susmitha</p>
+      <p><b>Course:</b> React Training</p>
+      <p><b>College:</b> ECM</p>
+      <p><b>City:</b> Hyderabad</p>
+
+      <hr />
+
+      <h2>My Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+
+      <hr />
+
+        </div>
+    );
+
+}
+export default personal
